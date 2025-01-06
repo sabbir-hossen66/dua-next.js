e9c79192bc31dd4b3c536dc67494414e43d68f96
sabbir-hossen66 <sabbir.ms2000@gmail.com>
2025-01-07 00:10:37 +0600

    creating navbar component

--- src/app/landing/page.jsx
+++ src/app/landing/page.jsx
@@ -1,9 +1,11 @@
+import Navbar from '@/components/navbar/Navbar';
 import React from 'react';
 
 const LandingPage = () => {
   return (
     <div>
       <h3>hello home</h3>
+      <Navbar />
     </div>
   );
 };
